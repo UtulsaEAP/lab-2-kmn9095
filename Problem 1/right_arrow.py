244881e1760d298458aa0974f08b92047ff2f143
Kelsey N <kmn9095@utulsa.edu>
2024-02-02 15:43:14 -0600

    Lab2.1

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -4,12 +4,15 @@ def right_arrow():
 
     row1 = '      ' + head_char
     ''' Type your code here. '''
+    row2 = base_char*6 + head_char*2
+    row3 = base_char*6 + head_char*3
+    row4 = base_char*6 + head_char
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
 
 if __name__ == "__main__":
     right_arrow()
--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,12 @@
 
 def caffeine():
+    
     caffeine_mg = float(input())
     ''' Type your code here. '''
-    
+    half_life1 = caffeine_mg/2
+    half_life2 = caffeine_mg/4
+    half_life3 = caffeine_mg/8
+    your_value = "After 6 hours: " + half_life1 + "mg", " After 12 hours: " + half_life2 + "mg", " After 24 hours:  " + half_life3 + "mg"
+    print(f'{your_value:.2f}')
 if __name__ == "__main__":
     caffeine()
