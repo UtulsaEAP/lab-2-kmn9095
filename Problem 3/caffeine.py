4f80e29008a51d742367d73590f6d4b968ed6922
Kelsey N <kmn9095@utulsa.edu>
2024-02-09 21:08:40 -0600

    Lab2.3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -11,9 +11,9 @@ def caffeine():
     caff_lvl24 = initial_quantity * (math.pow(0.5,4))
 
     
-    print('After 6 hours:' + " " + f'{caff_lvl6:.2f}' +  " " +'mg')
-    print('After 12 hours:' + " " + f'{caff_lvl12:.2f}' + " " + 'mg')
-    print('After 18 hours:' + " " + f'{caff_lvl24:.2f}' + " " + 'mg')
+    print("After 6 hours:" " " + f'{caff_lvl6:.2f}' +  " " +'mg')
+    print("After 12 hours:" " " + f'{caff_lvl12:.2f}' + " " + 'mg')
+    print("After 24 hours:" " " f'{caff_lvl24:.2f}' + " " + 'mg')
     
     #your_value = x , y , z  
     #print(f'{your_value:.2f}')
