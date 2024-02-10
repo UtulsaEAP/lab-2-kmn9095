d64faaf62fe2876189b49e5e079a0b8265853066
Kelsey N <kmn9095@utulsa.edu>
2024-02-09 19:21:34 -0600

    Lab2.2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,17 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
+    firstDig = str(int(phone_number / 10000000))
+    print(firstDig)
+    
+    phone_number = phone_number % 10000000
+    print(phone_number)
+    
+    middleDig = str(int(phone_number / 10000))
+    
+    lastDig = str(int(phone_number %10000))
+    
+    print("(" + firstDig + ") "+ middleDig + "-" + lastDig)
+    
 if __name__ == "__main__":
     telephone()
